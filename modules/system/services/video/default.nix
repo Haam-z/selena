2ba6f0fb4c0b8482397699fa8a74fca34cedcd01
Hamza Zarrouk <hamzazarouk@gmail.com>
2023-04-09 16:05:14 +0100

    back to the old school home manager config

--- modules/system/services/video/default.nix
+++ modules/system/services/video/default.nix
@@ -1,12 +1,14 @@
 { lib, pkgs, config, ... }:
 with lib;
 let cfg = config.selena.system.services.video;
-in {
+in
+{
   options.selena.system.services.video = {
     enable = mkEnableOption "enable my configuration of video";
   };
   config = mkIf cfg.enable {
     services.xserver.videoDrivers = [ "intel" ];
+    hardware.opengl.enable = true;
     hardware.opengl.driSupport32Bit = true;
   };
 }
